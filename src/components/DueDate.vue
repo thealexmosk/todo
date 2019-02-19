9bb01a1a4eca61d20fc7122a85681896d1445093
Alex Mosk <thealexmosk@gmail.com>
2019-02-19 18:04:08 +0200

    Datetime Component dependency for DueDate

--- src/components/DueDate.vue
+++ src/components/DueDate.vue
@@ -4,30 +4,45 @@
       Due Date
     </div>
     <div class="todo-btn__container">
-      <div class="time-field__val due-date">
-        <span>{{ date }}</span>
+      <div class="time-field__val due-date" @dblclick="openDatetime">
+        <span>{{ dateStr }}</span>
       </div>
-      <TodoButton type="edit"/>
+      <TodoButton type="edit" @click.native="openDatetime"/>
+      <Datetime v-model="date" type='datetime' input-id="datetime"/>
     </div>
   </div>
 </template>
 
 <script>
 import TodoButton from '@/components/TodoButton.vue'
+import {Datetime} from 'vue-datetime'
+import 'vue-datetime/dist/vue-datetime.css'
 
 export default {
   components: {
-    TodoButton
+    TodoButton,
+    Datetime
   },
   props: {
     dueDate: Date,
     extended: Boolean,
   },
+  data() {
+    return {
+      isEditing: false,
+      date: this.dueDate,
+    }
+  },
   computed: {
-    date() {
-      const date = this.dueDate || 'Due ?'
+    dateStr() {
+      const date = this.date || 'Due ?'
 
       return date
+    }
+  },
+  methods: {
+    openDatetime() {
+      document.getElementById('datetime').click()
     }
   }
 }
@@ -36,4 +51,12 @@ export default {
 <style lang="sass" scoped>
   .due-date
     background-color: rgba(78, 186, 111, 1)
+</style>
+
+<!-- Overriding Datetime classes -->
+<style lang="sass">
+  #datetime
+    display: none
+  .vdatetime-popup
+    border-radius: 5px
 </style>
